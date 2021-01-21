e19205458b4d24e8c01a48fab9763a8c2b289bb4
Simone Ripamonti <simone.ripamonti@bitrock.it>
2021-01-21 10:10:44 +0100

    fix consul interface in consul.sh

--- ansible/roles/hc-stack-install/files/consul.sh
+++ ansible/roles/hc-stack-install/files/consul.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-exec /usr/local/bin/consul agent -bind='{{ GetInterfaceIP \"'\$(ip route | grep '^default' | grep -Po '(?<=dev )(.+?)(?= )')'\" }}' -config-dir=/etc/consul.d/
+INTERFACE=$(ip route | grep '^default' | grep -Po '(?<=dev )(.+?)(?= )')
+exec /usr/local/bin/consul agent -bind="{{ GetInterfaceIP '$INTERFACE' }}" -config-dir=/etc/consul.d/
