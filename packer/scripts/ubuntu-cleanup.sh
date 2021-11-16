9a370e576d9285c6e9cabfbc382f85c94c1ba2d5
Andrea Simonini <andrea.simonini@gmail.com>
2021-11-16 15:46:51 +0100

    update cleanup

--- packer/scripts/ubuntu-cleanup.sh
+++ packer/scripts/ubuntu-cleanup.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 set -e
 sudo bash -c '{
-# clean yum history
-yum history new && \
-yum -y clean all  && \
-rm -rf /var/cache/yum && \
-rm /var/lib/rpm/.*.lock && \
-rpm --rebuilddb
+# clean apt cache
+apt clean 
 }'
 sudo bash -c '{
 # clean up logs
-truncate -c -s 0 /var/log/yum.log && \
 find /var/log -type f -exec truncate -s 0 {} \; && \
-rm -rf /var/log/anaconda* /var/lib/yum/* /var/lib/random-seed /root/install.log /root/install.log.syslog /root/anaconda-ks.cfg /etc/vault.d/watched
+rm -rf /etc/vault.d/watched
 }'
